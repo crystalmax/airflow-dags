29c5976c465f28fd0ae2fe3da53d26a38ecd5842
edwinguo <altergzj@yahoo.com>
2021-05-26 20:10:06 -0400

    Update the emr cluster id for the in class demo

--- spark_job.py
+++ spark_job.py
@@ -40,7 +40,7 @@ DEFAULT_ARGS = {
     'email_on_retry': False
 }
 
-CLUSTER_ID = 'j-1GXRYOF446YM'
+CLUSTER_ID = 'j-1CYSNRP3Q0FRC'
 
 def retrieve_s3_file(**kwargs):
     s3_location = kwargs['dag_run'].conf['s3_location'] 
