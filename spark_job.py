5b3da2a9fd36cc1928d109b006c690968507502c
Qixuan(Max) Ma <51385490+crystalmax@users.noreply.github.com>
2021-12-06 18:52:28 -0500

    Added emr default conn

--- spark_job.py
+++ spark_job.py
@@ -130,6 +130,7 @@ cluster_creator = EmrCreateJobFlowOperator(
         task_id='create_job_flow',
         job_flow_overrides=JOB_FLOW_OVERRIDES,
         aws_conn_id='aws_default',
+        emr_conn_id='emr_default',
         dag=dag
 )
 
